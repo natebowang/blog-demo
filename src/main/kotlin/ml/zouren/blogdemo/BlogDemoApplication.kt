993eede72c0d6575397b95cd54c0134dd412b900
natebowang <120029790@qq.com>
2021-02-20 13:33:52 +1300

    Init

---
+++ src/main/kotlin/ml/zouren/blogdemo/BlogDemoApplication.kt
@@ -0,0 +1,11 @@
+package ml.zouren.blogdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BlogDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<BlogDemoApplication>(*args)
+}
---
+++ src/test/kotlin/ml/zouren/blogdemo/BlogDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package ml.zouren.blogdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlogDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

b34904db687919cb17aa0c3ba08f30baab7bffb0
natebowang <120029790@qq.com>
2021-02-21 21:44:06 +1300

    Add README.md

---
+++ README.md
@@ -0,0 +1,17 @@
+## Kotlin Spring Boot Demo
+
+This is a simple Kotlin REST API server built by Spring Boot. 
+
+This API implements four [endpoints](https://app.swaggerhub.com/apis/natebowang/kotlin-spring_boot_demo/1.0.0):
+* GET /blogs
+* POST /blogs
+* GET /blogs/{id}
+* DELETE /blogs/{id}
+
+[This project](https://github.com/natebowang/blog-demo) utilized following technologies:
+* Kotlin
+* Spring Web
+* Spring Data JPA
+* Junit
+* Mockito
+* PostgreSQL
--- src/test/kotlin/ml/zouren/blogdemo/controller/BlogControllerTests.kt
+++ src/test/kotlin/ml/zouren/blogdemo/controller/BlogControllerTests.kt
@@ -1,11 +1,9 @@
 package ml.zouren.blogdemo.controller
 
-import ml.zouren.blogdemo.entity.Blog
 import ml.zouren.blogdemo.entity.BlogInput
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.http.MediaType
